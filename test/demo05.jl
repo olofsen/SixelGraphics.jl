7d53c9254657d7f4e692da0b9dab864bf1c08f91
Erik Olofsen <e.olofsen@xs4all.nl>
2014-02-14 17:55:21 +0100

    julia set demo

---
+++ test/demo05.jl
@@ -0,0 +1,32 @@
+using SixelGraphics
+
+# inspiration from
+# http://mathemartician.blogspot.nl/2012/07/julia-set-in-julia.html
+
+function julia(z, maxiter::Int64)
+    c=-0.75+0.11im
+    for n = 1:maxiter
+        if abs(z) > 2
+            return n-1
+        end
+        z = z^2 + c
+    end
+    return maxiter
+end
+
+w = 384
+h = 288
+
+s = Screen(w,h)
+
+clear(s.b,63)
+
+# for every pixel
+for y=1:h, x=1:w
+    # translate numbers [1:w, 1:h] -> -2.5:2.5 + -1:1 im
+    c = complex((x-w/2)/(h/2.5), (y-h/2)/(h/2))
+    # call the julia function and plot the distance
+    putpixel(s.b, div(julia(c,255),16), x-1, y-1)
+end
+
+sixels(s)
